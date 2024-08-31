032650b049fe608d2351767c16f820aec371f631
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-31 10:33:36 +0600

    18_5-1 store more than one value in a variable using array

---
+++ array.js
@@ -0,0 +1,12 @@
+// const num1 = 12;
+// const num2 = 22;
+// const num3 = 442;
+// const num4 = 442;
+
+// const numbers = 12342354365;
+// (), {}, []
+
+const numbers = [12, 22, 54, 65];
+const friends = ['abul', 'babul', 'cabul', 'dabul', 'bulbul'];
+const passed = [true, false, true, false];
+const mixedArray = [12, 'Alahbad',  true, 45,566]

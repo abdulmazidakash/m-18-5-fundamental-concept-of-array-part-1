6295e32b034842f579309303e07cfeb0f0bf3210
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-08-31 12:22:03 +0600

    18_5-2,3,4  array push pop shift unshift

---
+++ index.js
@@ -0,0 +1,16 @@
+const numbers = [88, 32, 55, 66, 77, 88, 99, 54, 234];
+
+console.log(numbers);
+
+//get element value by index
+console.log(numbers[1]);
+
+const fourth = numbers[3];
+console.log(fourth);
+
+//set or update element value by index
+let abc = 45;
+abc = 99;
+
+numbers[1] = 999;
+console.log(numbers);
---
+++ length.js
@@ -0,0 +1,3 @@
+const numbers = [1, 12, 323, 32, 43, 43, 43, 564, 65, 654];
+
+console.log(numbers.length);
